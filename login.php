63ebe768c21872cca9e1417212c4ec603e35e6b0
Omar <101218351+OmarLopGem@users.noreply.github.com>
2026-03-30 19:03:45 -0400

    First commit
    Created multiple files, login still very empty

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Tickify | Login</title>
+</head>
+<body>
+    <header>
+        <img src="./images/tickify_logo.png" alt="tickify logo" class="header-logo">
+        <nav class="nav-bar">
+            <ul class="nav-bar-list">
+                <li class="nav-bar-item"><a href="">Login</a></li>
+                <li class="nav-bar-item"><a href="/signup.php">Sign up</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
